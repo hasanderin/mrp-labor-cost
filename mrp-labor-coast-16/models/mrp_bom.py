ed8b29d658cc1f53875c0c7e1e6484b0a6847844
hasanderin <derinhasan28@gmail.com>
2026-05-13 13:30:46 +0300

    update cost

--- mrp-labor-coast-16/models/mrp_bom.py
+++ mrp-labor-coast-16/models/mrp_bom.py
@@ -16,3 +16,20 @@ class MrpBom(models.Model):
         default=lambda self: self.env.company.currency_id,
         required=True,
     )
+
+    def _get_bom_data(self, bom, product, qty=1.0, parent_bom=None,
+                      product_info=False, ignore_stock=False):
+        data = super()._get_bom_data(
+            bom, product, qty=qty, parent_bom=parent_bom,
+            product_info=product_info, ignore_stock=ignore_stock,
+        )
+        if bom and bom.labor_cost:
+            labor = bom.currency_id._convert(
+                bom.labor_cost * qty,
+                self.env.company.currency_id,
+                self.env.company,
+                fields.Date.today(),
+            )
+            data['cost'] = data.get('cost', 0.0) + labor
+            data['labor_cost'] = labor
+        return data
